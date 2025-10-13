a21ea9675da1958da683e1b481ab02f49fe899b1
vigneshs-ccl <vigneshkumar.s@crystalcodelabs.com>
2025-10-13 09:46:56 +0530

    removed commented codes

--- src/app/modules/coach/survey/survey.spec.ts
+++ src/app/modules/coach/survey/survey.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Survey } from './survey';
+import { Survey } from '@app/modules/coach/survey/survey';
 
 describe('Survey', () => {
   let component: Survey;
